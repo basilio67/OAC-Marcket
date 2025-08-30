6d73b2ddd874ee5353a2abfbbafae7a9151f2de5
basilio67 <zilysmith147@gmail.com>
2025-08-30 00:52:01 -0700

    Neon qualification

--- models/index.js
+++ models/index.js
@@ -1,14 +1,20 @@
 const { Sequelize } = require('sequelize');
 
 // Use variáveis de ambiente para produção
-const dbName = process.env.DB_NAME || 'oacmarket';
-const dbUser = process.env.DB_USER || 'postgres';
-const dbPass = process.env.DB_PASS || 'basilio@123';
-const dbHost = process.env.DB_HOST || 'localhost';
+const dbName = process.env.DB_NAME || 'neondb';
+const dbUser = process.env.DB_USER || 'neondb_owner';
+const dbPass = process.env.DB_PASS || 'npg_PNF8dVAjDTX0';
+const dbHost = process.env.DB_HOST || 'ep-rough-wildflower-a2awxjsg-pooler.eu-central-1.aws.neon.tech';
 
 const sequelize = new Sequelize(dbName, dbUser, dbPass, {
   host: dbHost,
   dialect: 'postgres'
-});
+  dialectOptions: {
+        ssl: {
+              require: true,
+                    rejectUnauthorized: false
+                        }
+                          }
+                          });
 
 module.exports = sequelize;
