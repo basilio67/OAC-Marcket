d6c48ec7054fddc245a933bc232317dcd3f73e22
basilio67 <zilysmith147@gmail.com>
2025-08-31 22:00:18 -0700

    Implantação Neon3

--- models/index.js
+++ models/index.js
@@ -8,7 +8,7 @@ const dbHost = process.env.DB_HOST || 'ep-rough-wildflower-a2awxjsg-pooler.eu-ce
 
 const sequelize = new Sequelize(dbName, dbUser, dbPass, {
   host: dbHost,
-  dialect: 'postgre',
+  dialect: 'postgres',
   dialectOptions: {
         ssl: {
               require: true,
