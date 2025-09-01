f50582c3d8fed9621d0eacb2611a03d4e2e262eb
basilio67 <zilysmith147@gmail.com>
2025-08-31 23:35:49 -0700

    Segundo Neon

--- models/index.js
+++ models/index.js
@@ -3,8 +3,8 @@ const { Sequelize } = require('sequelize');
 // Use variáveis de ambiente para produção
 const dbName = process.env.DB_NAME || 'neondb';
 const dbUser = process.env.DB_USER || 'neondb_owner';
-const dbPass = process.env.DB_PASS || 'npg_PNF8dVAjDTX0';
-const dbHost = process.env.DB_HOST || 'ep-rough-wildflower-a2awxjsg-pooler.eu-central-1.aws.neon.tech';
+const dbPass = process.env.DB_PASS || 'npg_YwoI68WyVEjO';
+const dbHost = process.env.DB_HOST || 'ep-twilight-meadow-afj0soew-pooler.c-2.us-west-2.aws.neon.tech';
 
 const sequelize = new Sequelize(dbName, dbUser, dbPass, {
   host: dbHost,
