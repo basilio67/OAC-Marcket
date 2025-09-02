5ac1d8da3ba6d843f986ffed4d141401e62d6dce
Basilio Nhantumbo <zilysmith147@gmail.com>
2025-09-02 11:43:13 -0700

    sequelize

--- routes/index.js
+++ routes/index.js
@@ -103,10 +103,9 @@ router.post('/login', async (req, res) => {
                 // Redireciona para criar loja
                 return res.redirect('/loja/criar');
             }
-        } else {
-            // Comprador vai para a lista de produtos
-            return res.redirect('/produtos');
         }
+        // Comprador vai para a lista de produtos
+        return res.redirect('/produtos');
     } else {
         res.render('login', { erro: 'Usuário ou senha inválidos.' });
     }
@@ -322,4 +321,5 @@ router.get('/termos', (req, res) => {
     res.render('termos');
 });
 
+module.exports = router;
 module.exports = router;
