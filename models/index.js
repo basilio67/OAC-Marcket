b660be0463e6d80dda272e327409508611f70e7a
Basilio Nhantumbo <zilysmith147@gmail.com>
2025-09-03 02:46:27 -0700

    admin2

--- models/index.js
+++ models/index.js
@@ -6,13 +6,15 @@ const dbUser = process.env.DB_USER || 'neondb_owner';
 const dbPass = process.env.DB_PASS || 'npg_YwoI68WyVEjO';
 const dbHost = process.env.DB_HOST || 'ep-twilight-meadow-afj0soew-pooler.c-2.us-west-2.aws.neon.tech';
 
+console.log('Conectando ao banco:', dbName, dbUser, dbHost);
+
 const sequelize = new Sequelize(dbName, dbUser, dbPass, {
   host: dbHost,
   dialect: 'postgres',
   dialectOptions: {
-        ssl: {
-              require: true,
-                    rejectUnauthorized: false
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
                         }
                           }
                           });
