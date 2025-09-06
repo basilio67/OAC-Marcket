acbffb0f30b2323cdc4c6ec39cb7573aab48f7c7
Basilio Nhantumbo <zilysmith147@gmail.com>
2025-09-06 01:35:01 -0700

    duas chamadas

--- server.js
+++ server.js
@@ -36,6 +36,3 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
-app.listen(PORT, () => {
-    console.log(`Servidor rodando em http://localhost:${PORT}`);
-});
